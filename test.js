0cb711364fc1e63aa9bebefa235d457ccd0125a5
vidushisaxen <vidushisaxenabdn786@gmail.com>
2023-09-20 18:39:07 +0530

    main

---
+++ test.js
@@ -0,0 +1,30 @@
+// console.log("Hello World")
+
+// for(let a=1;a<=5;a++){
+//     console.log(a);
+// }
+// function add(a,b){
+//     return a*b;
+// }
+// let c=add(7,8);
+// console.log(c);
+
+// let ans="";
+// function reverse(str){
+//     for(let i=str.length-1;i>=0;i--){
+//          ans +=str.charAt(i);
+         
+//     }
+//     return ans;
+// }
+// let result = reverse("hii");
+// console.log(result);
+
+function num(a){
+    if(a%2==0)
+    console.log("Even")
+else
+console.log("Odd");
+}
+num(9);
+
