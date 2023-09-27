c7884fcd49531803da8401009d9f2e25c20df5c3
vidushisaxen <vidushisaxenabdn786@gmail.com>
2023-09-27 23:04:10 +0530

    main

--- test.js
+++ test.js
@@ -40,9 +40,9 @@
 // console.log(x);
 
 
-let arr= [7,8,34,78,56,34,54,12,32];
-let sum=0;
-for(let i=0;i<arr.length;i++){
-    sum+=arr[i];
-}
-console.log(sum);
+// let arr= [7,8,34,78,56,34,54,12,32];
+// let sum=0;
+// for(let i=0;i<arr.length;i++){
+//     sum+=arr[i];
+// }
+// console.log(sum);
